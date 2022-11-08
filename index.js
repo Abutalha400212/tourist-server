03d0adb439a8ccd0614799494aa91d2492bf9bc9
Abu Talha <abutalha141999@gmail.com>
2022-11-08 18:59:12 +0600

    hanlded get and post function

--- index.js
+++ index.js
@@ -17,33 +17,59 @@ const client = new MongoClient(uri, {
 async function dbClient() {
   try {
     await client.connect();
-    console.log('Db connected');
+    console.log("Db connected");
   } catch (error) {
     console.log(error);
   }
 }
 dbClient();
-const visitingPlace = client.db('tourist').collection('visiting')
-app.get('/details/:id',async(req,res)=>{
-  const {id} = req.params
+const visitingPlace = client.db("tourist").collection("visiting");
+const reviews = client.db("tourist").collection("review");
+app.get("/details/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await visitingPlace.findOne({_id:ObjectId(id)})
-    res.send(result)
+    const result = await visitingPlace.findOne({ _id: ObjectId(id) });
+    res.send(result);
   } catch (error) {
     console.log(error);
   }
-})
-
-app.get('/service',async(req,res)=>{ 
-  const limit = parseInt(req.query.limit)
-    const find =  visitingPlace.find({})
-    try {
-       const result = await find.limit(limit).toArray()
-       res.send(result)
-    } catch (error) {
-        console.log(error.message);
+});
+app.post("/review", async (req, res) => {
+  const query = req.body;
+  try {
+    const result = await reviews.insertOne(query);
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `review accepted from authority on ${result.insertedId}`,
+      });
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/review", async (req, res) => {
+  const query = { email: req.query.email };
+  const find = reviews.find(query);
+  try {
+    const result = await find.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/service", async (req, res) => {
+  const limit = parseInt(req.query.limit);
+  const find = visitingPlace.find({});
+  try {
+    const result = await find.limit(limit).toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 app.get("/", (req, res) => {
   res.send("Api is running now");
 });

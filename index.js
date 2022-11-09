18e7a180adf94dd8614c30d7d3cfdc62f04b47e7
Abu Talha <abutalha141999@gmail.com>
2022-11-09 12:03:46 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
@@ -13,7 +15,21 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+function jwtVerifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.send({ message: "Unauthorized Access" });
+  }
 
+  jwtToken.verify(token, process.env.JWT_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function dbClient() {
   try {
     await client.connect();
@@ -25,6 +41,13 @@ async function dbClient() {
 dbClient();
 const visitingPlace = client.db("tourist").collection("visiting");
 const reviews = client.db("tourist").collection("review");
+
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_TOKEN, { expiresIn: "1h" });
+  res.send({ token });
+});
+
 app.get("/details/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -34,35 +57,38 @@ app.get("/details/:id", async (req, res) => {
     console.log(error);
   }
 });
-app.post('/addservice',async(req,res)=>{
-  const query = req.body
+app.post("/addservice", async (req, res) => {
+  const query = req.body;
   try {
-    const result = await visitingPlace.insertOne(query)
-    if(result.insertedId){
+    const result = await visitingPlace.insertOne(query);
+    if (result.insertedId) {
       res.send({
         success: true,
-        message: `review accepted from authority on ${result.insertedId}`
-      })
+        message: `review accepted from authority on ${result.insertedId}`,
+      });
     }
   } catch (error) {
     console.log(error);
   }
 });
-app.patch('/update/:id',async(req,res)=>{
-  const {id} = req.params
+app.patch("/update/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await reviews.updateOne({_id:ObjectId(id)},{$set:req.body})
-    if(result.matchedCount){
+    const result = await reviews.updateOne(
+      { _id: ObjectId(id) },
+      { $set: req.body }
+    );
+    if (result.matchedCount) {
       res.send({
-        success:true,
-        message:'Your Data updated'
-      })
+        success: true,
+        message: "Your Data updated",
+      });
     }
   } catch (error) {
     console.log(err);
   }
-})
-app.get('/update/:id',async(req,res)=>{
+});
+app.get("/update/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const result = await reviews.findOne({ _id: ObjectId(id) });
@@ -70,7 +96,7 @@ app.get('/update/:id',async(req,res)=>{
   } catch (error) {
     console.log(error);
   }
-})
+});
 app.post("/review", async (req, res) => {
   const query = req.body;
   try {
@@ -86,34 +112,38 @@ app.post("/review", async (req, res) => {
   }
 });
 
-app.get("/review", async (req, res) => {
+app.get("/review", jwtVerifyToken, async (req, res) => {
   const query = { email: req.query.email };
   const find = reviews.find(query);
   try {
+    const decoded = req.decoded;
+    if (decoded.email !== req.query.email) {
+      res.status(403).send({ message: "Forbidden Access" });
+    }
+
     const result = await find.toArray();
-        res.send({
-          success: true,
-          data: result
-          })
-     
+    res.send({
+      success: true,
+      data: result,
+    });
   } catch (error) {
     console.log(error);
   }
 });
-app.delete('/review/:id',async(req,res)=>{
-  const {id} = req.params
+app.delete("/review/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await reviews.deleteOne({_id:ObjectId(id)})
-  if(result.deletedCount){
-    res.send({
-      success:true,
-      message:`Item deleted on id ${result._id}`
-    })
-  }
+    const result = await reviews.deleteOne({ _id: ObjectId(id) });
+    if (result.deletedCount) {
+      res.send({
+        success: true,
+        message: `Item deleted on id ${result._id}`,
+      });
+    }
   } catch (error) {
     console.log(error);
   }
-})
+});
 app.get("/service", async (req, res) => {
   const limit = parseInt(req.query.limit);
   const find = visitingPlace.find({});
